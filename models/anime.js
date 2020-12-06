1a8bbd719dea1271208d5e5340bf5f54c53fc8ad
rush <mack9206@gmail.com>
2020-12-07 04:48:02 +0500

    popGCGUYASIGIHAGMAP]

--- models/anime.js
+++ models/anime.js
@@ -1,10 +1,43 @@
 const mongoose = require('mongoose')
+const path = require('path')
+
+const imgPath = 'uploads/tImages'
 
 const animeSchema = new mongoose.Schema({
-    name: {
+    title: {
+        type: String,
+        required: true
+    },
+    descr: {
+        type: String,
+    },
+    eCount: {
+        type: Number,
+        required: true
+    },
+    rDate: {
+        type: Date,
+        required: true
+    },
+    fDate: {
+        type: Date
+    },
+    cDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    imgName: {
         type: String,
         required: true
     }
 })
 
+animeSchema.virtual('imgVPath').get(function() {
+    if (this.imgName != null) {
+        return path.join('/', imgPath, this.imgName)
+    }
+})
+
 module.exports = mongoose.model('Anime', animeSchema)
+module.exports.imgPath = imgPath
--- routes/animes.js
+++ routes/animes.js
@@ -1,15 +1,22 @@
 const express = require('express')
-const anime = require('../models/anime')
 const router = express.Router()
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 const Anime = require('../models/anime')
-
-router.get('/', async (req, res) => {
-    let searchOptions = {}
-    if (req.query.name != null && req.query !== '') {
-        searchOptions.name = new RegExp(req.query.name, 'i')
+const uploadPath = path.join('public', Anime.imgPath)
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
+const upload = multer({
+    dest: uploadPath,
+    fileFilter: (req, file, callback) => {
+        callback(null, imageMimeTypes.includes(file.mimetype))
     }
+})
+
+// All Animes Route
+router.get('/', async (req, res) => {
     try {
-        const animes = await Anime.find(searchOptions)
+        const animes = await Anime.find({})
         res.render('animes/index', {
             animes: animes,
             searchOptions: req.query
@@ -19,24 +26,51 @@ router.get('/', async (req, res) => {
     }
 })
 
-router.get('/new', (req, res) => {
-    res.render('animes/new', { anime: new Anime() })
+// New Anime Route
+router.get('/new', async (req, res) => {
+    renderNewPage(res, new Anime())
 })
 
-router.post('/', async (req,res) => {
+// Post Anime Route
+router.post('/', upload.single('cover'), async (req, res) => {
+    const fileName = req.file != null ? req.file.filename : null
     const anime = new Anime({
-        name: req.body.name
+        title: req.body.title,
+        descr: req.body.descr,
+        rDate: new Date(req.body.rDate),
+        fDate: new Date(req.body.fDate),
+        eCount: req.body.eCount,
+        imgName: fileName
     })
     try {
         const newAnime = await anime.save()
         // res.redirect('animes/${newAnime.id}')
         res.redirect('animes')
     } catch {
-        res.render('animes/new', {
-            anime: anime,
-            errorMessage: 'Error posting an anime'
-        })
+        if (anime.imgName != null) {
+            removeThumbnail(anime.imgName)
+        }
+        renderNewPage(res, anime, true)
     }
 })
+
+async function renderNewPage(res, anime, hasError = false) {
+    try {
+        const anime = new Anime()
+        const params = {
+            anime: anime
+        }
+        if (hasError) params.errorMessage = 'Error posting an anime'
+        res.render('animes/new', params)
+    } catch {
+        res.redirect('/animes')
+    }
+}
+
+function removeThumbnail(fileName) {
+    fs.unlink(path.join(uploadPath, fileName), err => {
+        if (err) console.log(err)
+    })
+}
 
 module.exports = router
