f15246afb48a8c73a181287b8f5595540fdc670d
zzz-zzz-zzz-zzz <55892833+zzz-zzz-zzz-zzz@users.noreply.github.com>
2020-12-08 08:31:02 +0500

    Add files via upload

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,13 @@
-  
+
 FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode,
-    FilePondPluginImageTransform
   )
-
-FilePond.setOptions ({
-
-})
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / 0.75,
+    imageResizeTargetWidth: 325,
+    imageResizeTargetHeight: 450
+  })
 
 FilePond.parse(document.body);
